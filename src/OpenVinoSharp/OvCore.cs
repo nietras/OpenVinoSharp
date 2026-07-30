a59e1469e6ce031218fbf1c902b0ac0374487fcb
nietras <nietras@users.noreply.github.com>
2026-07-30 10:43:14 +0200

    add OvProfilingInfo

--- src/OpenVinoSharp/OvCore.cs
+++ src/OpenVinoSharp/OvCore.cs
@@ -14,6 +14,14 @@ public sealed class OvCore : SafeHandle
 
     public OvModel ReadModel(string modelPath, string? binPath = null) => new(this, modelPath, binPath);
 
+    public void SetProperty(string deviceName, string propertyKey, string propertyValue)
+    {
+        ArgumentException.ThrowIfNullOrWhiteSpace(deviceName);
+        ArgumentException.ThrowIfNullOrWhiteSpace(propertyKey);
+        ArgumentNullException.ThrowIfNull(propertyValue);
+        Ov.ov_core_set_property(new Ov.CoreHandle(handle), deviceName, propertyKey, propertyValue).Ok();
+    }
+
     internal Ov.CoreHandle CoreHandle => new(handle);
 
     public override bool IsInvalid => handle == nint.Zero;
---
+++ src/OpenVinoSharp/OvProfilingInfo.cs
@@ -0,0 +1,16 @@
+﻿namespace OpenVinoSharp;
+
+public sealed record OvProfilingInfo(
+    OvProfilingStatus Status,
+    long RealTimeMicroseconds,
+    long CpuTimeMicroseconds,
+    string NodeName,
+    string ExecutionType,
+    string NodeType);
+
+public enum OvProfilingStatus
+{
+    NotRun,
+    OptimizedOut,
+    Executed,
+}
